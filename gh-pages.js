564d06faf04421f4795f70cf3d07f3cf8b7554c1
Eugence1117 <eugencelim@gmail.com>
2022-01-25 23:09:56 +0800

    Change branch

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ var ghpages = require('gh-pages');
 ghpages.publish(
     'public', // path to public directory
     {
-        branch: 'gh-pages',
+        branch: 'main',
         repo: 'https://github.com/eugencelim/eugencelim.github.io', // Update to point to your repository  
         user: {
             name: 'eugencelim', // update to use your name
