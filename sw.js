af9d4a10569657caaf3502fdc7edae46b2b425f3
eugencelim <eugence.lim@fusionexgroup.com>
2022-01-28 10:18:30 +0800

    Updates

--- sw.js
+++ sw.js
@@ -14,6 +14,19 @@ self.addEventListener('install',function(e){
     )
 })
 
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.open('static_res').then(function(cache) {
+        return cache.match(event.request).then(function (response) {
+          return response || fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
+
 //Activate
 self.addEventListener('activate',function(){
     console.log("service worker: activated.")
